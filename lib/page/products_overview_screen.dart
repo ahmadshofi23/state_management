f94c270b490aca6b2868007023fe7fbe55ba76f3
ahmadshofi23 <ahmadshofihasibuan12@gmail.com>
2022-01-09 01:13:51 +0700

    add cart provider

--- lib/page/products_overview_screen.dart
+++ lib/page/products_overview_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_management/page/cart_page.dart';
+import 'package:state_management/providers/cart_provider.dart';
+import 'package:state_management/widget/badge.dart';
 import '../widget/product_grid.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
@@ -7,6 +11,25 @@ class ProductsOverviewScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
+        actions: [
+          Consumer<CartProvider>(
+            builder: (context, value, ch) {
+              return Badge(
+                child: ch,
+                value: value.jumlah.toString(),
+              );
+            },
+            child: IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(
+                  CartPage.routeName,
+                  // arguments: productData.id,
+                );
+              },
+              icon: Icon(Icons.shopping_cart),
+            ),
+          ),
+        ],
       ),
       body: ProductGrid(),
     );
